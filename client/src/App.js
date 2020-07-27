407a1a8e40be086e111150cf584d14b8f39e0c71
kswamen <kswamen@naver.com>
2020-07-27 17:26:50 +0900

    200727 1726

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
 import Appbar from "./components/Appbar";
 import Scroller from "./components/Scroller";
-import { makeStyles } from "@material-ui/core/styles";
 import { Link, animateScroll as scroll } from "react-scroll";
+import { withStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
+const styles = (theme) => ({
   firstDiv: {
     backgroundColor: "#81c147",
     width: "100vw",
@@ -20,21 +20,22 @@ const useStyles = makeStyles((theme) => ({
     width: "100vw",
     height: "100vh",
   },
-}));
+});
 
-function App() {
-  const classes = useStyles();
-
-  return (
-    <div>
-      <div id="section1" className={classes.firstDiv}>
-        <Appbar />
-        <Scroller />
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <div id="section1" className={classes.firstDiv}>
+          <Appbar />
+          <Scroller />
+        </div>
+        <div id="section2" className={classes.secondDiv}></div>
+        <div id="section3" className={classes.thirdDiv}></div>
       </div>
-      <div id="section2" className={classes.secondDiv}></div>
-      <div id="section3" className={classes.thirdDiv}></div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
