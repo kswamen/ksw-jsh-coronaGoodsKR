ae6ce327c021da27800c4b0166a5dff924a68512
kswamen <kswamen@naver.com>
2020-08-05 16:21:12 +0900

    200805 1621 ㅏㄴㅈ

--- client/src/components/Appbar.js
+++ client/src/components/Appbar.js
@@ -1,146 +1,154 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 import imgMask from "../image/CoronaIcon.png";
-import Typing from "react-typing-animation";
 import { Link, animateScroll as scroll } from "react-scroll";
 
 import Login from "./Login";
 
-const useStyles = makeStyles((theme) => ({
+const styles = (theme) => ({
   root: {
     flexGrow: 1,
+    display: "flex",
+    alignItems: "center",
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
-}));
+});
 
-export default function Appbar() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
+class Appbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: null,
+      isLoggedIn: false,
+    };
+  }
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  render() {
+    const { classes } = this.props;
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClick = (event) => {
+      this.setState({ anchorEl: event.currentTarget });
+    };
 
-  return (
-    <div className={classes.root}>
-      <AppBar
-        position="fixed"
-        style={{ backgroundColor: "rgba( 52, 52, 52, 0.8)" }}
-      >
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <img style={{ width: 80, height: 70 }} src={imgMask} />
-          </IconButton>
-
-          <Typography variant="h4" color="#ffffff">
-            CoronaGoodsKR
-          </Typography>
-
-          <div
-            style={{
-              display: "flex",
-              marginLeft: "auto",
-              position: "relative",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Button color="default">
-              <Link
-                activeClass="active"
-                to="section1"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Typography
-                  style={{ color: "#ffffff", padding: "15px" }}
-                  variant="h5"
-                >
-                  Total Patients
-                </Typography>
-              </Link>
-            </Button>
-            <Button color="default">
-              <Link
-                activeClass="active"
-                to="section2"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Typography
-                  style={{ color: "#ffffff", padding: "15px" }}
-                  variant="h5"
-                >
-                  Products
-                </Typography>
-              </Link>
-            </Button>
-            <Button color="default">
-              <Link
-                activeClass="active"
-                to="section3"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Typography
-                  style={{
-                    color: "#ffffff",
-                    padding: "15px",
-                    marginRight: "15px",
-                  }}
-                  variant="h5"
-                >
-                  News
-                </Typography>
-              </Link>
-            </Button>
-            <Button
-              aria-controls="simple-menu"
-              aria-haspopup="true"
-              onClick={handleClick}
-              variant="outlined"
-              color="default"
-              style={{ borderColor: "#ffffff", height: "30%" }}
+    const handleClose = () => {
+      this.setState({ anchorEl: null });
+    };
+    return (
+      <div className={classes.root}>
+        <AppBar
+          position="fixed"
+          style={{ backgroundColor: "rgba( 52, 52, 52, 0.8)", display: "flex" }}
+        >
+          <Toolbar>
+            <div
+              style={{
+                width: "100vw",
+                display: "flex",
+                position: "relative",
+                justifyContent: "space-between",
+                alignItems: "center",
+                flexDirection: "row",
+              }}
             >
-              <Typography style={{ color: "#ffffff" }} variant="h5">
-                Login
-              </Typography>
-            </Button>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>
-                <Login />
-              </MenuItem>
-            </Menu>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+              <div
+                style={{
+                  width: "30vw",
+                  display: "flex",
+                  position: "relative",
+                  alignItems: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <IconButton
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="menu"
+                  style={{ alignSelf: "flex-start" }}
+                >
+                  <img style={{ width: 80, height: 70 }} src={imgMask} />
+                </IconButton>
+                <Typography variant="h4" color="#ffffff">
+                  CoronaGoodsKR
+                </Typography>
+              </div>
+              <div
+                style={{
+                  width: "50vw",
+                  display: "flex",
+                  position: "relative",
+                  justifyContent: "flex-end",
+                  flexDirection: "row",
+                  marginLeft: "auto",
+                }}
+              >
+                <Button color="default">
+                  <Link
+                    activeClass="active"
+                    to="section1"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                  >
+                    <Typography
+                      style={{ color: "#ffffff", padding: "15px" }}
+                      variant="h5"
+                    >
+                      Total Patients
+                    </Typography>
+                  </Link>
+                </Button>
+                <Button color="default">
+                  <Link
+                    activeClass="active"
+                    to="section2"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                  >
+                    <Typography
+                      style={{ color: "#ffffff", padding: "15px" }}
+                      variant="h5"
+                    >
+                      Products
+                    </Typography>
+                  </Link>
+                </Button>
+                <Button color="default">
+                  <Link
+                    activeClass="active"
+                    to="section3"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                  >
+                    <Typography
+                      style={{
+                        color: "#ffffff",
+                        padding: "15px",
+                        marginRight: "15px",
+                      }}
+                      variant="h5"
+                    >
+                      News
+                    </Typography>
+                  </Link>
+                </Button>
+              </div>
+              <Login />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
+
+export default withStyles(styles)(Appbar);
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,38 +1,123 @@
-import React, { Component } from "react";
-import { GoogleLogin } from "react-google-login";
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
 
-export default class login extends Component {
+export default class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       id: "",
       name: "",
       provider: "",
+      anchorEl: null,
+      isLoggedIn: false,
     };
   }
 
-  responseGoogle = (res) => {
-    this.setState({
-      id: res.googleId,
-      name: res.profileObj.name,
-      provider: "google",
-    });
-    console.log(res);
-    console.log(this.state.id);
-    console.log(this.state.name);
-  };
-
   render() {
+    const handleClick = (event) => {
+      this.setState({ anchorEl: event.currentTarget });
+    };
+
+    const handleClose = () => {
+      this.setState({ anchorEl: null });
+    };
+
+    const responseGoogleOnSuccess = (res) => {
+      this.setState({
+        id: res.googleId,
+        name: res.profileObj.name,
+        provider: "google",
+        img: res.profileObj.imageUrl,
+        isLoggedIn: true,
+      });
+      console.log(res);
+      console.log(this.state.id);
+      console.log(this.state.name);
+    };
+
+    const responseGoogleOnFailure = (res) => {
+      console.log(res);
+    };
+
+    const logoutPressed = () => {
+      this.setState({ isLoggedIn: false });
+      console.log(this.state.isLoggedIn);
+    };
+
     return (
-      <GoogleLogin
-        clientId={
-          "310814871301-d992m2477iodmeta9hlpo9jnthjv1gfj.apps.googleusercontent.com"
-        }
-        onSuccess={this.responseGoogle}
-        onFailure={this.responseGoogle}
-        cookiePolicy={"single_host_origin"}
-        isSignedIn={true}
-      ></GoogleLogin>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginLeft: "auto",
+        }}
+      >
+        {!this.state.isLoggedIn ? (
+          ""
+        ) : (
+          <Button
+            style={{
+              width: "50px",
+              height: "50px",
+            }}
+          >
+            <img
+              style={{
+                width: "100%",
+                height: "100%",
+                borderRadius: "50%",
+                overflow: "auto",
+              }}
+              src={this.state.img}
+            />
+          </Button>
+        )}
+        <Button
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+          variant="outlined"
+          color="default"
+          style={{ borderColor: "#ffffff", height: "30%" }}
+        >
+          <Typography style={{ color: "#ffffff" }} variant="h5">
+            {!this.state.isLoggedIn ? "Login" : "Logout"}
+          </Typography>
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={this.state.anchorEl}
+          keepMounted
+          open={Boolean(this.state.anchorEl)}
+          onClose={handleClose}
+        >
+          {!this.state.isLoggedIn ? (
+            <MenuItem onClick={handleClose}>
+              <GoogleLogin
+                clientId={
+                  "310814871301-d992m2477iodmeta9hlpo9jnthjv1gfj.apps.googleusercontent.com"
+                }
+                onSuccess={responseGoogleOnSuccess}
+                onFailure={responseGoogleOnFailure}
+                cookiePolicy={"single_host_origin"}
+                isSignedIn={true}
+              ></GoogleLogin>
+            </MenuItem>
+          ) : (
+            <MenuItem onClick={handleClose}>
+              <GoogleLogout
+                buttonText="로그아웃하시겠습니까?"
+                onLogoutSuccess={logoutPressed}
+              ></GoogleLogout>
+            </MenuItem>
+          )}
+        </Menu>
+      </div>
     );
   }
 }
