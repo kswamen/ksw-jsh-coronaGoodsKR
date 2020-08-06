8a6f770958195fec81282fe8897bef5311f63c20
kswamen <kswamen@naver.com>
2020-08-06 16:09:06 +0900

    Router

--- client/src/pages/bulletinBoard.js
+++ client/src/pages/bulletinBoard.js
@@ -1,22 +1,31 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import NewsCarousel from "../components/NewsCarousel";
+import Posts from "../components/Posts";
+import bg1 from "../image/bg1.jpeg";
+import AppbarBBS from "../components/AppbarBBS";
 
-class BBS extends Component {
+const styles = (theme) => ({
+  firstDiv: {
+    backgroundImage: "url(" + bg1 + ")",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "auto",
+    height: "100vh",
+  },
+});
+
+class bulletinBoard extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignContent: "center",
-          padding: 30,
-        }}
-      >
-        <div>
-          <h2>Contact Page</h2>
-        </div>
-      </div>
+      <>
+        <AppbarBBS />
+        <div id="section1" className={classes.firstDiv}></div>
+      </>
     );
   }
 }
 
-export default BBS;
+export default withStyles(styles)(bulletinBoard); //withStyles 덕분에 firstDiv가 구별된다.
