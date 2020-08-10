737d1d5cd61bbf3daa8ada176e893885d5ad8eac
kswamen <kswamen@naver.com>
2020-08-10 13:59:55 +0900

    ksw

--- client/src/components/BulletinBoardMain.js
+++ client/src/components/BulletinBoardMain.js
@@ -1,33 +1,71 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import "../css/carousel.css";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const styles = (theme) => ({
+  table: {
+    minWidth: 700,
+  },
+});
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
 
 class BulletinBoardMain extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      newsData: [],
-    };
-  }
-
-  componentDidMount() {
-    console.log(this.props.AppbarHeight);
+    this.state = {};
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <Button
-        style={{
-          top: this.props.AppbarHeight,
-          color: "#ffffff",
-          width: "500px",
-          height: "500px",
-        }}
+      <TableContainer
+        style={{ top: this.props.AppbarHeight + 15, position: "relative" }}
+        component={Paper}
       >
-        <h1>abcde</h1>
-      </Button>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>No.</StyledTableCell>
+              <StyledTableCell>게시명</StyledTableCell>
+              <StyledTableCell>작성자</StyledTableCell>
+              <StyledTableCell>게시일자</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <StyledTableRow>
+              <StyledTableCell>a</StyledTableCell>
+              <StyledTableCell>a</StyledTableCell>
+              <StyledTableCell>a</StyledTableCell>
+              <StyledTableCell>a</StyledTableCell>
+            </StyledTableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     );
   }
 }
 
-export default BulletinBoardMain;
+export default withStyles(styles)(BulletinBoardMain);
